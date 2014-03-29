daddd687357a1f4bd610cebd017fd7f5123e3f75
Richo Healey <richo@psych0tik.net>
2014-03-29 20:01:18 +1100

    Implement Repository.path()
    I believe this doesn't leak

--- src/repository.rs
+++ src/repository.rs
@@ -1,17 +1,30 @@
 use std::libc::{c_void,c_char};
 use types::{GitCache,GitAttrCache,GIT_CVAR_CACHE_MAX};
 use std::mem;
+use std::c_str::CString;
 
 pub mod types;
-
 
 struct Repository {
     repo: *mut c_void,
 }
 
+impl Repository {
+    fn path(&self) -> Option<~str> {
+        unsafe {
+            let path = git_repository_path(self.repo);
+            match CString::new(path, true).as_str() {
+                Some(s) => return Some(s.to_owned()),
+                None => return None
+            }
+        }
+    }
+}
+
 #[link(name="git2")]
 extern {
     fn git_repository_open(repo: **mut c_void, path: &str) -> u8;
+    fn git_repository_path(repo: *mut c_void) -> *c_char;
 }
 
 pub fn new_repository(path: &str) -> Option<Repository> {
